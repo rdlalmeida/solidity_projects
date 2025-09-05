541c94733b8deaaa82131c2b12df2cbba8a68445
Ricardo Almeida <rdlalmeida@gmail.com>
2025-09-05 17:48:50 +0100

    Test synch

--- 23_car_factory/contracts/CarFactory.sol
+++ 23_car_factory/contracts/CarFactory.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.24;
 
 // Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
-contract Lock {
+contract CarFactory {
     uint public unlockTime;
     address payable public owner;
 
