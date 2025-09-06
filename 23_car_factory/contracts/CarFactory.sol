7546e56af764b72da235f4e0c271627d1cccb3b8
Ricardo Almeida <rdlalmeida@gmail.com>
2025-09-06 01:18:09 +0100

    Done with the solidity car example

--- 23_car_factory/contracts/CarFactory.sol
+++ 23_car_factory/contracts/CarFactory.sol
@@ -5,30 +5,81 @@ pragma solidity ^0.8.24;
 import "hardhat/console.sol";
 
 contract CarFactory {
-    uint public unlockTime;
-    address payable public owner;
+    uint256 private nextCarId;
+    address public factoryOwner;
 
-    event Withdrawal(uint amount, uint when);
+    mapping(uint256 carId => address) private carOwner; 
+    mapping(uint256 carId => string) public carColor;
+    mapping(uint256 carId => string) public licencePlate;
+    mapping(uint256 carId => bool) public runningCar;
+    mapping(uint256 carId => string) private insurancePolicy;
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+    event CarCreated(uint256 _carId);
+    event CarStarted(uint256 _carId);
+    event CarStopped(uint256 _carId);
+    event CarDestroyed(uint256 _carId);
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    constructor() {
+        nextCarId = 1;
+        factoryOwner = msg.sender;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    /**
+     * Function to create a new car asset
+     * @param newCarColor The color of the car to build.
+     * @param newLicencePlate The licence plate to add to the car asset
+     * @param newInsurancePolicy The insurance policy associated to the car built.
+     * @return uint256 The carId of the new car.
+     */
+    function createCar(address newOwner, string memory newCarColor, string memory newLicencePlate, string memory newInsurancePolicy) public returns(uint256) {
+        require(msg.sender == factoryOwner, "You don't own this car factory!");
+        // Save the current carId and increment the counter at the end for the next car
+        uint256 currentCarId = nextCarId++;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        // Fill out the mappings
+        carOwner[currentCarId] = newOwner;
+        carColor[currentCarId] = newCarColor;
+        licencePlate[currentCarId] = newLicencePlate;
+        runningCar[currentCarId] = false;
+        insurancePolicy[currentCarId] = newInsurancePolicy;
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+        emit CarCreated(currentCarId);
+        return currentCarId;
+    }
 
-        owner.transfer(address(this).balance);
+    function startCar(uint256 carId) public {
+        require(carOwner[carId] == msg.sender, "You don't own this car!");
+
+        if (!runningCar[carId]) {
+            runningCar[carId] = true;
+        }
+
+        emit CarStarted(carId);
+    }
+
+    function stopCar(uint256 carId) public {
+        require(carOwner[carId] == msg.sender, "You don't own this car!");
+
+        if (runningCar[carId]) {
+            runningCar[carId] = false;
+        }
+
+        emit CarStopped(carId);
+    }
+
+    function isCarRunning(uint256 carId) public view returns(bool) {
+        return runningCar[carId];
+    }
+
+    function destroyCar(uint256 carId) public {
+        require(carOwner[carId] == msg.sender, "You don't own this car!");
+
+        delete insurancePolicy[carId];
+        delete runningCar[carId];
+        delete licencePlate[carId];
+        delete carColor[carId];
+        delete carOwner[carId];
+
+        emit CarDestroyed(carId);
     }
 }
